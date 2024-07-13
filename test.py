7f862a3c4e23e2947ef05c5646bcbe6e2bc1d86d
Wajih Humayun Hashmi <wajihh57@gmail.com>
2024-07-13 15:37:42 +0100

    Initial Commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print("Hello World")
 print("Somehow it Works")
 print("Learnt using git through terminal for version control")
-Print("Summary of commands to run python file added to README")
+print("Summary of commands to run python file added to README")
